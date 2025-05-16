e11e19c21ab937467e49576b3042c17e11154c53
Stanley Cheung <stanleycheung@google.com>
2025-05-16 21:15:33 +0000

    script

--- scripts/dp-test.py
+++ scripts/dp-test.py
@@ -1,8 +1,22 @@
 import socket
 
-try:
-    sock = socket.create_connection(("2001:4860:8040:0826:0000:00ad:0b0d:8a5a", 22), timeout=6)
-    print("successful")
-    sock.close()
-except Exception as e:
-    print(f"connection failed: {e}")
+addresses = [
+    "2001:4860:8040:0826:0000:00ac:b958:8958",
+    "2001:4860:8040:0826:0000:00ac:ffaa:e78b",
+    "2001:4860:8040:0826:0000:00ac:e853:9134",
+    "2001:4860:8040:0826:0000:00ad:0625:a92d",
+    "2001:4860:8040:0826:0000:00ad:0437:1d09",
+    "2001:4860:8040:0826:0000:00ac:b959:90ab",
+    "2001:4860:8040:0826:0000:00ac:f78f:f705",
+    "2001:4860:8040:0826:0000:00ac:b959:9345",
+    "2001:4860:8040:0826:0000:00ac:b959:938b",
+]
+
+for address in addresses:
+    try:
+        print(f"trying {address}")
+        sock = socket.create_connection((address, 22), timeout=5)
+        print("successful")
+        sock.close()
+    except Exception as e:
+        print(f"connection failed: {e}")
