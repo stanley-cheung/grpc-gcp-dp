bfd332afcadaac7aaa513e80cfa9de8b9fc83ca8
Stanley Cheung <stanleycheung@google.com>
2025-05-16 21:33:58 +0000

    fix port

--- scripts/dp-test.py
+++ scripts/dp-test.py
@@ -1,23 +1,8 @@
 import socket
 
-addresses = [
-    "2001:4860:8040:0826:0000:00ac:b958:8958",
-    "2001:4860:8040:0826:0000:00ac:ffaa:e78b",
-    "2001:4860:8040:0826:0000:00ac:e853:9134",
-    "2001:4860:8040:0826:0000:00ad:0625:a92d",
-    "2001:4860:8040:0826:0000:00ad:0437:1d09",
-    "2001:4860:8040:0826:0000:00ac:b959:90ab",
-    "2001:4860:8040:0826:0000:00ac:f78f:f705",
-    "2001:4860:8040:0826:0000:00ac:b959:9345",
-    "2001:4860:8040:0826:0000:00ac:b959:938b",
-]
-
-for address in addresses:
-    try:
-        print(f"trying {address}")
-        with socket.socket(socket.AF_INET6, socket.SOCK_STREAM) as s:
-            sock = s.connect((address, 22, 0, 0))
-            print("successful")
-            sock.close()
-    except Exception as e:
-        print(f"connection failed: {e}")
+try:
+    sock = socket.create_connection(("2001:4860:8040:0826:0000:00ac:b96d:04df", 14507), timeout=5)
+    print("successful")
+    sock.close()
+except Exception as e:
+    print(f"connection failed: {e}")
