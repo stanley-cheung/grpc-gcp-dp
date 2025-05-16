8e355ac1d5274dc112332380f0e57e5ccacd454a
Stanley Cheung <stanleycheung@google.com>
2025-05-16 20:44:18 +0000

    one ipv6 wv address

--- scripts/dp-test.py
+++ scripts/dp-test.py
@@ -1,7 +1,7 @@
 import socket
 
 try:
-    sock = socket.create_connection(("www.google.com", 80), timeout=5)
+    sock = socket.create_connection(("2001:4860:8040:0826:0000:00ac:b953:bbae", 22), timeout=10)
     print("successful")
     sock.close()
 except Exception as e:
