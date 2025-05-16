4439efe7560a46d1d0bc7ba305a2bfc763bebb6a
Stanley Cheung <stanleycheung@google.com>
2025-05-16 20:51:00 +0000

    one ipv6 wv address

--- scripts/dp-test.py
+++ scripts/dp-test.py
@@ -1,7 +1,7 @@
 import socket
 
 try:
-    sock = socket.create_connection(("2001:4860:8040:0826:0000:00ac:b953:bbae", 22), timeout=10)
+    sock = socket.create_connection(("2001:4860:8040:0826:0000:00ad:0b0d:8a5a", 22), timeout=6)
     print("successful")
     sock.close()
 except Exception as e:
